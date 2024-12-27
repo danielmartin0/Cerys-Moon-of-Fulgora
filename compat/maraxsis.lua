0df883e3f54674adbf3260a2833f4f09ede03b39
danielmartin0 <github@danielgmartin.com>
2024-12-27 01:20:31 +0000

    see changelog.txt

---
+++ compat/maraxsis.lua
@@ -0,0 +1,10 @@
+if mods["maraxsis"] then
+	if data.raw.recipe["maraxsis-petroleum-gas-cracking"] then
+		data.raw.recipe["maraxsis-petroleum-gas-cracking"].surface_conditions = {
+			{
+				property = "temperature",
+				min = 253,
+			},
+		}
+	end
+end
