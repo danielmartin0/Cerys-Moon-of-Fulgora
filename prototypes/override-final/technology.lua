deaca768da6d712b45fafa0f319b4effb19f193c
snouz <snooz-x@hotmail.com>
2025-01-11 22:00:40 +0100

    further icon edits

--- prototypes/override-final/technology.lua
+++ prototypes/override-final/technology.lua
@@ -113,7 +113,7 @@ data:extend({
 		},
 		prerequisites = { "cerys-science-pack" },
 		icon = "__Cerys-Moon-of-Fulgora__/graphics/technology/advanced-structure-repair.png",
-		icon_size = 767,
+		icon_size = 256,
 	}),
 	cerys_tech({
 		science_count = 300,
