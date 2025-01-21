49cf0fbc707c613b0cbec2a76aeec2e3c6ffa486
danielmartin0 <github@danielgmartin.com>
2025-01-21 00:01:50 +0000

    Revert "see changelog.txt"
    This reverts commit d069b2994bee33bc17f581e44ff3ebdcc88ae9d0.

--- prototypes/entity/player-nuclear-reactor.lua
+++ prototypes/entity/player-nuclear-reactor.lua
@@ -1,23 +1,17 @@
+local merge = require("lib").merge
+
 local max_health = 2000
 
-local reactor_sound = sound_variations("__base__/sound/nuclear-reactor", 2, 1)
-for _, sound in pairs(reactor_sound) do
-	sound.speed = 0.9
-end
-
-local mixed_oxide_reactor = {
-	type = "reactor",
+local mixed_oxide_reactor = merge(data.raw.reactor["nuclear-reactor"], {
 	name = "cerys-mixed-oxide-reactor",
-	icon = "__Cerys-Moon-of-Fulgora__/graphics/icons/fulgoran-reactor.png",
-	icon_size = 64,
-	flags = { "placeable-neutral", "player-creation" },
-	minable = { mining_time = 1, result = "cerys-mixed-oxide-reactor" },
-	fast_replaceable_group = "mixed-oxide-reactor",
+	subgroup = "energy",
+	order = "z-f[nuclear-energy]-a[reactor]-a[mixed-oxide-reactor]",
 	max_health = max_health,
-	corpse = "nuclear-reactor-remnants",
-	dying_explosion = "nuclear-reactor-explosion",
-	consumption = "160MW",
-	neighbour_bonus = 1,
+	collision_box = { { -3.2, -3.2 }, { 3.2, 3.2 } },
+	selection_box = { { -3.5, -3.5 }, { 3.5, 3.5 } },
+	minable = { mining_time = 1, result = "cerys-mixed-oxide-reactor" },
+	lower_layer_picture = "nil",
+	heat_lower_layer_picture = "nil",
 	energy_source = {
 		type = "burner",
 		fuel_categories = { "nuclear-mixed-oxide" },
@@ -30,19 +24,9 @@ local mixed_oxide_reactor = {
 			maximum_intensity = 0.95,
 		},
 	},
-	collision_box = { { -3.2, -3.2 }, { 3.2, 3.2 } },
-	selection_box = { { -3.5, -3.5 }, { 3.5, 3.5 } },
-	damaged_trigger_effect = data.raw["reactor"]["nuclear-reactor"].damaged_trigger_effect,
-	lower_layer_picture = "nil",
-	heat_lower_layer_picture = "nil",
-	subgroup = "energy",
-	order = "z-f[nuclear-energy]-a[reactor]-a[mixed-oxide-reactor]",
-
-	heat_buffer = {
-		max_temperature = 1500,
-		specific_heat = "10MJ",
-		max_transfer = "40GW",
-		minimum_glow_temperature = 175,
+	consumption = "160MW", -- From 40MW
+	heat_buffer = merge(data.raw.reactor["nuclear-reactor"].heat_buffer, {
+		minimum_glow_temperature = 175, -- From 350
 		connections = {
 			{
 				position = { -3, -3 },
@@ -118,8 +102,10 @@ local mixed_oxide_reactor = {
 			blend_mode = "additive",
 			draw_as_glow = true,
 		}),
-	},
-
+		specific_heat = "10MJ", -- from 10MJ
+		max_temperature = 1500, -- from 1000
+		max_transfer = "40GW", -- from 10GW
+	}),
 	connection_patches_connected = {
 		sheet = {
 			filename = "__Cerys-Moon-of-Fulgora__/graphics/entity/player-nuclear-reactor/reactor-connect-patches-4x4.png",
@@ -140,7 +126,6 @@ local mixed_oxide_reactor = {
 		}),
 	},
 	heat_connection_patches_disconnected = "nil",
-
 	working_light_picture = {
 		blend_mode = "additive",
 		draw_as_glow = true,
@@ -151,7 +136,6 @@ local mixed_oxide_reactor = {
 		shift = util.by_pixel(35, -8),
 		tint = { r = 0, g = 1, b = 1 },
 	},
-
 	picture = {
 		layers = {
 			{
@@ -171,34 +155,31 @@ local mixed_oxide_reactor = {
 			},
 		},
 	},
-
-	impact_category = "metal-large",
-	open_sound = { filename = "__base__/sound/open-close/nuclear-open.ogg", volume = 0.8 },
-	close_sound = { filename = "__base__/sound/open-close/nuclear-close.ogg", volume = 0.8 },
-	working_sound = reactor_sound,
-
-	meltdown_action = {
-		type = "direct",
-		action_delivery = {
-			type = "instant",
-			target_effects = {
-				{
-					type = "create-entity",
-					entity_name = "atomic-rocket",
-				},
-			},
-		},
-	},
-
-	default_temperature_signal = { type = "virtual", name = "signal-T" },
-	circuit_wire_max_distance = reactor_circuit_wire_max_distance,
-	circuit_connector = circuit_connector_definitions["nuclear-reactor"],
-
+	icon = "__Cerys-Moon-of-Fulgora__/graphics/icons/fulgoran-reactor.png",
+	icon_size = 64,
+	-- lower_layer_picture = {
+	-- 	filename = "__Cerys-Moon-of-Fulgora__/graphics/entity/nuclear-reactor/Reactor-pipes-0.5.png",
+	-- width = 2113,
+	-- height = 2068,
+	-- scale = 0.55,
+	-- shift = ?,
+	-- },
+	-- heat_lower_layer_picture = apply_heat_pipe_glow({
+	-- 	filename = "__Cerys-Moon-of-Fulgora__/graphics/entity/nuclear-reactor/Reactor-pipes-heated-0.5.png",
+	-- width = 2113,
+	-- height = 2068,
+	-- scale = 0.55,
+	-- shift = ?,
+	-- }),
+	-- working_sound = merge(data.raw.reactor["nuclear-reactor"].working_sound, {
+	-- 	sound = sound_variations("__base__/sound/nuclear-reactor", 2, 1),
+	-- }),
+	fast_replaceable_group = "mixed-oxide-reactor",
 	surface_conditions = { {
 		property = "magnetic-field",
 		max = 119,
 	} },
-}
+})
 
 data:extend({
 	mixed_oxide_reactor,
