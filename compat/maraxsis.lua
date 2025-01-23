d44f13330e508ff113071ef0c824a8a6e00bd5ac
danielmartin0 <github@danielgmartin.com>
2025-01-23 01:58:43 +0000

    see changelog.txt

--- compat/maraxsis.lua
+++ compat/maraxsis.lua
@@ -1,23 +1,23 @@
-local override_surface_conditions = require("lib").override_surface_conditions
+local restrict_surface_conditions = require("lib").restrict_surface_conditions
+local relax_surface_conditions = require("lib").relax_surface_conditions
 
 if mods["maraxsis"] then
 	if data.raw.recipe["maraxsis-petroleum-gas-cracking"] then
-		override_surface_conditions(data.raw.recipe["maraxsis-petroleum-gas-cracking"], {
+		restrict_surface_conditions(data.raw.recipe["maraxsis-petroleum-gas-cracking"], {
 			property = "temperature",
 			min = 255,
 		})
 	end
 
 	if data.raw.recipe["rocket-part"].surface_conditions then
-		for _, surface_condition in pairs(data.raw.recipe["rocket-part"].surface_conditions) do
-			if surface_condition.property == "gravity" and surface_condition.min > 0.1 then
-				surface_condition.min = 0.1
-			end
-		end
+		relax_surface_conditions(data.raw.recipe["rocket-part"], {
+			property = "gravity",
+			min = 0.1,
+		})
 	end
 
 	if data.raw.recipe["maraxsis-sublimation"] then
-		override_surface_conditions(data.raw.recipe["maraxsis-sublimation"], {
+		restrict_surface_conditions(data.raw.recipe["maraxsis-sublimation"], {
 			property = "temperature",
 			min = 255,
 		})
