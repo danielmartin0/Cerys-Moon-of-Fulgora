5fc732422830ac4c04730d75ba3f9982b7aa5258
danielmartin0 <github@danielgmartin.com>
2025-03-16 02:01:33 +0000

    see changelog.txt

--- compat/compat.lua
+++ compat/compat.lua
@@ -19,6 +19,32 @@ data.raw.lab["cerys-lab"].next_upgrade = nil
 data.raw.reactor["cerys-fulgoran-reactor"].next_upgrade = nil
 data.raw["assembling-machine"]["cerys-fulgoran-cryogenic-plant"].next_upgrade = nil
 
+--== Fix mods installing faulty next_upgrade on Cerys entities (5-dim, etc): ==--
+
+local collision_mask_util = require("collision-mask-util")
+
+for prototype in pairs(defines.prototypes.entity) do
+	for _, entity in pairs(data.raw[prototype] or {}) do
+		if string.sub(entity.name, 1, 6) == "cerys-" or string.sub(entity.name, 1, 21) == "radiative-tower-lamp-" then
+			local next_upgrade = data.raw[prototype][entity.next_upgrade or ""]
+			if next_upgrade then
+				local collision_mask_1 = collision_mask_util.get_mask(entity)
+				local collision_mask_2 = collision_mask_util.get_mask(next_upgrade)
+				if not collision_mask_util.masks_are_same(collision_mask_1, collision_mask_2) then
+					entity.next_upgrade = nil
+				end
+
+				if entity.fast_replaceable_group ~= next_upgrade.fast_replaceable_group then
+					entity.next_upgrade = nil
+				end
+			end
+			if not entity.minable then
+				entity.next_upgrade = nil
+			end
+		end
+	end
+end
+
 --== Prevent burner inserters from being restricted on Cerys: ==--
 
 if data.raw["inserter"]["burner-inserter"] then
