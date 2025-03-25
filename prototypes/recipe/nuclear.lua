268f0036eb4862292d1629768a4fbf6d64e06ae2
danielmartin0 <github@danielgmartin.com>
2025-03-25 21:07:45 +0000

    see changelog.txt

--- prototypes/recipe/nuclear.lua
+++ prototypes/recipe/nuclear.lua
@@ -94,7 +94,7 @@ data:extend({
 			{ type = "item", name = "uranium-235", amount = 1, probability = 50 / 100 },
 			{ type = "item", name = "uranium-238", amount = (50 / 100 * common.REPROCESSING_U238_TO_U235_RATIO) },
 			{ type = "item", name = "plutonium-238", amount = 1 },
-			{ type = "item", name = "plutonium-239", amount = 5 },
+			{ type = "item", name = "plutonium-239", amount = 5, extra_count_fraction = 0.5 },
 		},
 		subgroup = "plutonium-processing",
 		order = "c-c",
