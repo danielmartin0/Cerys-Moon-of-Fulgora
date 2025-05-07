31460e6030a78d4507582486d9ebdb204e1d7f92
danielmartin0 <github@danielgmartin.com>
2025-05-07 17:05:27 +0100

    see changelog.txt

--- compat/forced-mods.lua
+++ compat/forced-mods.lua
@@ -1,5 +1,5 @@
 if mods["lignumis"] and not mods["cerys-lunaponics"] then
 	error(
-		"Playing Cerys alongside Lignumis requires installing the mod Wooden Cerys: Lunaponics (https://mods.factorio.com/mod/cerys-lunaponics). Please download and install this mod from the Mod Portal."
+		"\n\nPlaying Cerys alongside Lignumis requires installing the mod Wooden Cerys: Lunaponics (https://mods.factorio.com/mod/cerys-lunaponics).\n\nPlease download and install this mod from the Mod Portal.\n"
 	)
 end
