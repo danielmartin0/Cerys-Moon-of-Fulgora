2a648e64c31550a8d4a8bb55a7419051dcf76138
danielmartin0 <github@danielgmartin.com>
2025-06-03 15:47:31 +0100

    see changelog.txt

--- prototypes/planet/map-gen.lua
+++ prototypes/planet/map-gen.lua
@@ -93,6 +93,20 @@ data:extend({
 
 	{
 		type = "noise-expression",
+		name = "cerys_ash_light",
+		expression = "multioctave_noise{\z
+			x = cerys_x_surface, \z
+			y = cerys_y_surface, \z
+			seed0 = map_seed, \z
+			seed1 = 6400, \z
+			octaves = 5, \z
+			persistence = 0.7, \z
+			input_scale = 1 / 2, \z
+			output_scale = 1}",
+	},
+
+	{
+		type = "noise-expression",
 		name = "cerys_ash_cracks",
 		expression = "multioctave_noise{\z
 			x = cerys_x_surface, \z
