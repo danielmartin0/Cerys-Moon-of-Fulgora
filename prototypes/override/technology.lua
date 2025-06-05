0c652d559f37faf6de80f3612b1ed3a1ae441223
danielmartin0 <github@danielgmartin.com>
2025-06-05 17:41:12 +0100

    see changelog.txt

--- prototypes/override/technology.lua
+++ prototypes/override/technology.lua
@@ -1,14 +1,28 @@
 if settings.startup["cerys-infinite-braking-technology"].value then
-	if
-		data.raw.technology["braking-force-7"]
-		and not (
-			data.raw.technology["braking-force-7"].max_level
-			and data.raw.technology["braking-force-7"].max_level == "infinite"
-		)
-	then
-		data.raw.technology["braking-force-7"].max_level = "infinite"
-		data.raw.technology["braking-force-7"].unit.count_formula = "2^(L-7)*"
-			.. data.raw.technology["braking-force-7"].unit.count
-		data.raw.technology["braking-force-7"].unit.count = nil
-	end
+	data:extend({
+		{
+			type = "technology",
+			name = "braking-force-8",
+			icons = util.technology_icon_constant_braking_force("__base__/graphics/technology/braking-force.png"),
+			effects = {
+				{
+					type = "train-braking-force-bonus",
+					modifier = 0.15,
+				},
+			},
+			prerequisites = { "braking-force-7" },
+			unit = {
+				count_formula = "2^(L-7)*" .. data.raw.technology["braking-force-7"].unit.count,
+				ingredients = {
+					{ "automation-science-pack", 1 },
+					{ "logistic-science-pack", 1 },
+					{ "chemical-science-pack", 1 },
+					{ "production-science-pack", 1 },
+					{ "utility-science-pack", 1 },
+				},
+				time = 60,
+			},
+			upgrade = true,
+		},
+	})
 end
