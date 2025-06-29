8e42d4d665fd3cf4a46cdd425a1f6b353c651da9
danielmartin0 <github@danielgmartin.com>
2025-06-29 20:13:59 +0100

    see changelog.txt

--- common-data-only.lua
+++ common-data-only.lua
@@ -1,7 +1,8 @@
 local Public = {}
 
 Public.K2_INSTALLED = mods["Krastorio2-spaced-out"] or mods["Krastorio2"]
-Public.NITRIC_ACID_NAME = Public.K2_INSTALLED and "kr-nitric-acid" or "nitric-acid"
+-- Public.NITRIC_ACID_NAME = Public.K2_INSTALLED and "kr-nitric-acid" or "nitric-acid"
+Public.NITRIC_ACID_NAME = "kr-nitric-acid"
 Public.LITHIUM_NAME = Public.K2_INSTALLED and "kr-lithium" or "lithium"
 
 return Public
