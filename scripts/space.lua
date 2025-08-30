9de84cdb274733a4dc3b934f98403bdc8feb7acd
danielmartin0 <github@danielgmartin.com>
2025-08-30 20:54:01 +0100

    Update space.lua

--- scripts/space.lua
+++ scripts/space.lua
@@ -19,7 +19,7 @@ local NORMALIZATION_DISTANCE = 5 -- Given the deflection strength, changing the 
 local SPEED_THRESHOLD = 0.05 * (2 / 3)
 local PARTICLE_SHRINK_TIME = 14
 
-local CHANCE_DAMAGE_CHARACTER = common.HARD_MODE_ON and 1 or 0.015
+local CHANCE_DAMAGE_CHARACTER = common.HARD_MODE_ON and 1 or 0.0125
 
 local CHANCE_MUTATE_BELT_URANIUM = 1 / 760
 local CHANCE_MUTATE_INVENTORY_URANIUM = 1 / 7600
