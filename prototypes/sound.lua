2e270b8017cf8393911a4d855c32362002567427
danielmartin0 <github@danielgmartin.com>
2025-08-31 21:28:26 +0100

    see changelog.txt

--- prototypes/sound.lua
+++ prototypes/sound.lua
@@ -189,18 +189,6 @@ data:extend({
 	},
 	{
 		type = "ambient-sound",
-		name = "cerys-8",
-		track_type = "main-track",
-		weight = 10,
-		planet = "cerys",
-		sound = {
-			filename = "__Cerys-Moon-of-Fulgora__/sounds/horizons.ogg",
-			volume = 0.5,
-			priority = 200,
-		},
-	},
-	{
-		type = "ambient-sound",
 		name = "cerys-9",
 		track_type = "main-track",
 		weight = 10,
