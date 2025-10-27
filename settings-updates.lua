905c009644968a363f445d84f9cecb900bc9aaf8
danielmartin0 <github@danielgmartin.com>
2025-10-27 00:47:24 +0000

    portmod

--- settings-updates.lua
+++ settings-updates.lua
@@ -1,1 +1,5 @@
 data.raw["bool-setting"]["PlanetsLib-enable-temperature"].forced_value = true
+
+if data.raw["bool-setting"]["portmod-add-to-fulgora-tech-tree"] then
+	data.raw["bool-setting"]["portmod-add-to-fulgora-tech-tree"].default_value = false
+end
