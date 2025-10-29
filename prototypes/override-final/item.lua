9bea74be4036684762a00cf35dde61a44f40680b
danielmartin0 <github@danielgmartin.com>
2025-10-29 20:09:18 +0000

    see changelog.txt

--- prototypes/override-final/item.lua
+++ prototypes/override-final/item.lua
@@ -2,8 +2,6 @@ data.raw.item["solid-fuel"].fuel_category = "chemical-or-radiative"
 
 -- NOTE: Do we still need this now that we can make frozen tile variants?
 data.raw.item["stone-brick"].place_as_tile.condition.layers["cerys_tile"] = true
-data.raw.item["stone-brick"].place_as_tile.condition_size = data.raw.item["stone-brick"].place_as_tile.condition_size
-	+ 1
 
 table.insert(data.raw.item["foundation"].place_as_tile.tile_condition, "cerys-water-puddles")
 table.insert(data.raw.item["foundation"].place_as_tile.tile_condition, "cerys-water-puddles-freezing")
