b051affa73f286438ca27bd6f4d65c377f80fe2a
danielmartin0 <github@danielgmartin.com>
2025-12-16 23:21:21 +0000

    lignumis update

--- compat/forced-mods.lua
+++ compat/forced-mods.lua
@@ -1,5 +1,5 @@
 local has_WU_mods = mods["wood-logistics"] and mods["fulgora-coralmium-agriculture"]
-local has_lignumis = mods["lignumis"]
+local has_lignumis_directors_cut = mods["lignumis-cf"]
 local has_lunaponics = mods["cerys-lunaponics"]
 
 if has_WU_mods and not has_lunaponics then
@@ -8,8 +8,8 @@ if has_WU_mods and not has_lunaponics then
 	)
 end
 
-if has_lignumis and not has_lunaponics then
+if has_lignumis_directors_cut and not has_lunaponics then
 	error(
-		"\n\nPlaying Cerys alongside Lignumis requires installing the mod Wooden Cerys: Lunaponics (https://mods.factorio.com/mod/cerys-lunaponics).\n\nPlease download and install this mod from the Mod Portal.\n"
+		"\n\nPlaying Cerys alongside Lignumis: Director's Cut requires installing the mod Wooden Cerys: Lunaponics (https://mods.factorio.com/mod/cerys-lunaponics).\n\nPlease download and install this mod from the Mod Portal.\n"
 	)
 end
