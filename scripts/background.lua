a434bb903d0e4753b2a102dabba3c7e710b1c02f
danielmartin0 <github@danielgmartin.com>
2026-07-26 22:48:45 +0100

    Don't assert on a missing background sprite

--- scripts/background.lua
+++ scripts/background.lua
@@ -6,9 +6,13 @@ local Public = {}
 local PLANET_OFFSET = { x = 50, y = -30 }
 local PLANET_PARALLAX = 0.35
 
-assert(helpers.is_valid_sprite_path("cerys-fulgora-background"),"Sprite path 'cerys-fulgora-background' is invalid.")
+local background_sprite_valid = nil
 
 function Public.tick_1_update_background_renderings(surface)
+	if background_sprite_valid == nil then
+		background_sprite_valid = helpers.is_valid_sprite_path("cerys-fulgora-background")
+	end
+
 	if game.is_multiplayer() and settings.global["cerys-disable-parallax-in-multiplayer"].value then
 		if not storage.parallax_disabled then
 			Public.reset_background_rendering_positions()
@@ -27,7 +31,7 @@ function Public.tick_1_update_background_renderings(surface)
 			local on_cerys = player.surface.name == "cerys"
 			local r = storage.background_renderings[player.index]
 
-			if on_cerys then
+			if on_cerys and background_sprite_valid then
 				local stretch = lib.get_cerys_surface_stretch_factor(surface)
 				local planet_stretch = stretch
 				local extra_y_offset = -5 * (stretch - 1)
