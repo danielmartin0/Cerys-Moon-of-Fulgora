d794bee0e7a6d0e3a80728846c2130b7ad262e7e
Nicholas Gower <nicholasgower01@gmail.com>
2026-07-29 12:59:32 -0700

    Fixed migration

--- migrations/4.24.9.lua
+++ migrations/4.24.9.lua
@@ -1,4 +1,6 @@
 local collisions = require("scripts.collisions")
+local init = require("scripts.init")
+init.ensure_top_level_storage()
 if game.planets.cerys then
     local surface = game.planets.cerys.surface
     for _,entity in pairs(surface.find_entities_filtered{type = {"transport-belt","container","logistic-container"}}) do
